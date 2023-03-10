2a770d64a6a982e353429e2cdf398c21559a5855
Yashveer singh <yashveersingh30401@gmail.com>
2023-03-10 17:56:51 +0530

    1. Shared Flow Added

--- app/src/main/java/com/lifecycleawarecomponents/FlowActivity.kt
+++ app/src/main/java/com/lifecycleawarecomponents/FlowActivity.kt
@@ -2,23 +2,19 @@ package com.lifecycleawarecomponents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.lifecycleawarecomponents.databinding.ActivityFlowBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 import java.util.concurrent.Flow
 
 class FlowActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityFlowBinding
+    private lateinit var binding: ActivityFlowBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityFlowBinding.inflate(layoutInflater)
+        binding = ActivityFlowBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
@@ -33,14 +29,14 @@ class FlowActivity : AppCompatActivity() {
             val listNumbers = flowProducer()
             listNumbers.collect {
                 GlobalScope.launch(Dispatchers.Main) {
-                binding.textFlow.text=it.toString()
+                    binding.textFlow.text = it.toString()
                 }
             }
         }
     }
 
-    private fun flowProducer()=flow<Int> {
-        val listOfnumbers = listOf(1,2,3,4,5,6,7,8)
+    private fun flowProducer() = flow<Int> {
+        val listOfnumbers = listOf(1, 2, 3, 4, 5, 6, 7, 8)
 
         listOfnumbers.forEach {
             delay(1000)
@@ -48,37 +44,55 @@ class FlowActivity : AppCompatActivity() {
         }
     }
 
-    private fun producePersonFlow() = flow<person>{
-        val person1 = person("Yash",21)
-        val person2 = person("Mohit",19)
-        val person3 = person("Sandeep",27)
-        val person4 = person("Akshat",20)
+    private fun producePersonFlow() = flow<person> {
 
-        val listOfPersons = listOf<person>(person1,person2,person3,person4)
+        val person1 = person("Yash", 21)
+        val person2 = person("Mohit", 19)
+        val person3 = person("Sandeep", 27)
+        val person4 = person("Akshat", 20)
+
+        val listOfPersons = listOf<person>(person1, person2, person3, person4)
 
         listOfPersons.forEach {
             delay(1000)
             emit(it)
+            throw Exception("ERROR In Emitter")
         }
 
+
+
+    }.catch {
+        Log.d("Error","Error In Producer")
+        emit(person("null",-1))
     }
 
-    private fun consumePersonflow(){
+    private fun consumePersonflow() {
         GlobalScope.launch {
-            val personList = producePersonFlow()
-            personList
-                .map {
-                    person(it.personName.toString().toUpperCase(),it.personAge)
-                }
-                .filter {
-                    it.personAge!! >20
-                }
-                .collect{
-                GlobalScope.launch(Dispatchers.Main) {
-                    binding.personName.text = it.personName.toString()
-                    binding.personAge.text = it.personAge.toString()
-                }
+            Log.d("THREAD", "consumePersonflow: ${Thread.currentThread().name}")
+            try {
+                val personList = producePersonFlow()
+                personList
+                    .map {
+                        person(
+                            it.personName.toString().toUpperCase(),
+                            it.personAge
+                        ) //This map and filter operator executes on IO Thread
+                    }
+                    .filter {
+                        it.personAge!! > 20
+                    }
+                    .flowOn(Dispatchers.IO)
+
+                    .collect {
+                        GlobalScope.launch(Dispatchers.Main) {
+                            binding.personName.text = it.personName.toString()
+                            binding.personAge.text = it.personAge.toString()
+                        }
+                    }
+            } catch (e: Exception) {
+                Toast.makeText(this@FlowActivity, e.message.toString(), Toast.LENGTH_SHORT).show()
             }
+
         }
     }
 }
---
+++ app/src/main/java/com/lifecycleawarecomponents/SharedFlowPrac.kt
@@ -0,0 +1,11 @@
+package com.lifecycleawarecomponents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SharedFlowPrac : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shared_flow_prac)
+    }
+}
